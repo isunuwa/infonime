c5ddfc361e237e8174edd5b28a2524a47e4fbbe2
Israel Sunuwar <isunuwar@devfinity.io>
2023-09-04 16:12:31 +0545

    basic routing established

---
+++ src/app/_layouts/app-layout/app-layout.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppLayoutRoutingModule } from './app-layout-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppLayoutRoutingModule
+  ]
+})
+export class AppLayoutModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppLayoutModule } from './_layouts/app-layout/app-layout.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutModule,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./modules/login/login.module').then((m) => m.LoginModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
