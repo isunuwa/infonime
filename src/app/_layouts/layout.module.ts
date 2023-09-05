f363e7aae55cf6d58e223f3c3dd3f6c79378f89e
Israel Sunuwar <isunuwar@devfinity.io>
2023-09-05 11:27:49 +0545

    header component integrated

--- src/app/_layouts/layout.module.ts
+++ src/app/_layouts/layout.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { NavModule } from '../modules/navigation/nav.module';
 
 export const LAYOUT_COMPONENTS = [AppLayoutComponent];
 
 @NgModule({
   declarations: [...LAYOUT_COMPONENTS],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NavModule],
   exports: [...LAYOUT_COMPONENTS],
 })
 export class LayoutModule {}
