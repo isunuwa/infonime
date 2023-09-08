3853c342e8b31ad68fdc9e786b215a482b862405
Israel Sunuwar <isunuwar@devfinity.io>
2023-09-08 14:03:36 +0545

    detail page ui integrated

--- src/app/modules/anime/components/anime-detail/anime-detail.component.ts
+++ src/app/modules/anime/components/anime-detail/anime-detail.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { AnimeService } from '../../services/anime.service';
+import { Anime } from '../../models/anime.module';
 
 @Component({
   selector: 'app-anime-detail',
-  templateUrl: './anime-detail.component.html'
+  templateUrl: './anime-detail.component.html',
 })
-export class AnimeDetailComponent {
+export class AnimeDetailComponent implements OnInit {
+  anime!: Anime;
 
+  constructor(
+    private animeService: AnimeService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getAnimeDetail();
+  }
+
+  getAnimeDetail(): void {
+    const id = +this.route.snapshot.params['id'];
+
+    this.animeService.getAnimeDetail(id).subscribe((response) => {
+      console.log(response.data);
+
+      this.anime = response.data;
+    });
+
+    // this.animeService.getAnime(id).subscribe((anime) => (this.anime = anime));
+  }
 }
