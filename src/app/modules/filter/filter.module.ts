2d9b7b2cc152a736cce76b1f2531b4067840a9f7
Israel Sunuwar <isunuwar@devfinity.io>
2023-12-27 17:50:19 +0545

    basic ui integrated for filter ui

--- src/app/modules/filter/filter.module.ts
+++ src/app/modules/filter/filter.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FilterRoutingModule } from './filter-routing.module';
+import { FilterComponent } from './components/filter.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FilterComponent],
   imports: [
     CommonModule,
     FilterRoutingModule
-  ]
+  ],
+  exports: [FilterComponent]
 })
 export class FilterModule { }
